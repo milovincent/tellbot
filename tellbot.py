milovincent/tellbot

commit ba626e9f1dfdfa08a24c0cd9d2a28398956f5da5

Report pending messages for never-seen users from DB

tellbot.py
@@ -222,10 +222,14 @@ class NotificationDistributorSQLite(NotificationDistributor):
 
     def query_seen(self, user):
         with self.lock:
-            self.curs.execute('SELECT name, seen.timestamp, COUNT(text) '
-                'FROM seen LEFT JOIN messages ON user = recipient '
-                'AND delivered IS NULL WHERE user = ?', (user,))
-            return self.curs.fetchone()
+            self.curs.execute('SELECT name, seen.timestamp FROM seen '
+                'WHERE user = ?', (user,))
+            head = self.curs.fetchone()
+            if head is None: head = (None, None)
+            self.curs.execute('SELECT COUNT(*) FROM messages '
+                'WHERE recipient = ?', (user,))
+            tail = self.curs.fetchone()
+            return head + tail
 
     def update_seen(self, user, name, timestamp):
         with self:

commit efda1166a448df191ff3ba0d3c7f2f40afb72c2f

Do not report already-delivered messages in !seen
Fixes an issue introduced in ba626e9f1dfdfa08a24c0cd9d2a28398956f5da5.

tellbot.py
@@ -231,7 +231,7 @@ class NotificationDistributorSQLite(NotificationDistributor):
             head = self.curs.fetchone()
             if head is None: head = (None, None)
             self.curs.execute('SELECT COUNT(*) FROM messages '
-                'WHERE recipient = ?', (user,))
+                'WHERE recipient = ? AND delivered IS NULL', (user,))
             tail = self.curs.fetchone()
             return head + tail
 